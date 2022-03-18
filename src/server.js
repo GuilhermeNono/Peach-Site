d0e44b0734a33b918e71d51ddef61858cf87a2b0
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-03-18 17:52:40 -0300

    Convertendo a aplicação para Node.js

---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require('express');
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "../public" )));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.listen(PORT);
+console.log(`Server is running at http://localhost:${PORT}`);
