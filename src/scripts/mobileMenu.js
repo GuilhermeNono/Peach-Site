56641c7d0ff13f48369afb363dffc6c0fb2f3751
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-03-18 17:00:13 -0300

    Finalizando a Homepage

--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -1,13 +1,27 @@
-let show = true;
+let showMenu = true;
+let showImage = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const logo = document.querySelector(".logo-shake")
+const aboutH1 = document.querySelector(".about_text")
 
 menuToggle.addEventListener("click", () => {
 
-    document.body.style.overflow = show ? "hidden" : "initial";
+    document.body.style.overflow = showMenu ? "hidden" : "initial";
 
-    menuSection.classList.toggle("on", show);
+    menuSection.classList.toggle("on", showMenu);
 
-    show = !show;
+    showMenu = !showMenu;
 })
+
+if(window.matchMedia("(max-width: 425px)").matches){
+
+    logo.classList.toggle("off", showImage);
+
+    aboutH1.classList.toggle("on", showImage)
+    
+    showImage = !showImage
+
+
+}
