2601b717b3126e83218654148e3e4394201b81ca
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-03-19 16:55:42 -0300

    Vercel Port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, "/public" )));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.listen(PORT);
